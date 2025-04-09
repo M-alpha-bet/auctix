3bd3043b1901f8a63ae36546ac5a89c45da863e6
M-alpha-bet <fridayokeomamartin@gmail.com>
2025-04-09 02:23:26 +0100

    socials

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,16 +5,16 @@ export default function Footer() {
   return (
     <footer className="text-center pt-10 pb-3">
       <div className="flex gap-5 py-4 justify-center">
-        <Link href="/">
+        <Link href="https://x.com/martinfriday5?s=21">
           <FaXTwitter className="size-5" />
         </Link>
-        <Link href="/">
+        <Link href="https://github.com/M-alpha-bet/auctix">
           <FaGithub className="size-5" />
         </Link>
-        <Link href="/">
+        <Link href="https://www.instagram.com/martinelli_mabs?igsh=MXZldm04cHBlazBwaw%3D%3D&utm_source=qr">
           <FaInstagram className="size-5" />
         </Link>
-        <Link href="/">
+        <Link href="https://wa.me/2348144565996">
           <FaWhatsapp className="size-5" />
         </Link>
       </div>
